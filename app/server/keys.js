bcc9d719e9c6f0daf4cddcb5ad7269c012501e46
jefferson oliveira <cardbeatt@gmail.com>
2018-08-15 19:17:05 -0300

    keys for testing

---
+++ app/server/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    google: {
+        clientID:'1053976588809-6p4jgbm7hpk6jvrnsd8b5uhga2miipio.apps.googleusercontent.com',
+        clientSecret: 'qodZXqo_JOYuFo2ccwBCArwk'
+    }
+};
