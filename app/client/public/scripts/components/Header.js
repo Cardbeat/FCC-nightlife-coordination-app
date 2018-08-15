cb114d12262a8def52d954e1fdbcd6dd0dd4113b
jefferson oliveira <cardbeatt@gmail.com>
2018-08-15 18:24:03 -0300

    added passport, keys and google api

--- app/client/public/scripts/components/Header.js
+++ app/client/public/scripts/components/Header.js
@@ -6,24 +6,19 @@ export default class Header extends Component {
         return (
         <div>
             <nav>
-            <div className="nav-wrapper">
-                <a href="#!" className="brand-logo">Logo</a>
-                <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-                <ul className="right hide-on-med-and-down">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">Javascript</a></li>
-                    <li><a href="mobile.html">Mobile</a></li>
-                </ul>
-            </div>
-        </nav>
-
-        <ul className="sidenav" id="mobile-demo">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">Javascript</a></li>
-            <li><a href="mobile.html">Mobile</a></li>
-        </ul>
+                <div className="nav-wrapper blue darken-4">
+                    <a href="#!" className="brand-logo">NightLife</a>
+                    <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                    <ul className="right hide-on-med-and-down">
+                        <li><a href="/oauth/google">login with Google+</a></li>
+                        <li><a href="/oauth/logout">logout</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <ul className="sidenav" id="mobile-demo">
+                <li><a href="/oauth/google">login with Google</a></li>
+                <li><a href="/oauth/logout">logout</a></li>
+            </ul>
         </div>
         )
     }
