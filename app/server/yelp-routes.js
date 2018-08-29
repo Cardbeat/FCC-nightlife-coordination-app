b2b6b1d4813321c4ce864249b0cc67892e52ff3d
jefferson oliveira <cardbeatt@gmail.com>
2018-08-29 19:53:10 -0300

    get yelp business to react

--- app/server/yelp-routes.js
+++ app/server/yelp-routes.js
@@ -11,12 +11,15 @@ router.post('/search', (req, res) => {
     client.search({
         location: req.body.city
       }).then(response => {
-          res.redirect('/')
           res.send(response.jsonBody.businesses)
       }).catch(e => {
         console.log(e);
       });
 });
+
+// need to search about passing data 
+// get business info and alocate at redux store 
+// change path with react router
 
 
 module.exports = router;
