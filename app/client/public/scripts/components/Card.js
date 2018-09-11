a1360441622af2e91106f21e7d3a727b445de801
jefferson oliveira <cardbeatt@gmail.com>
2018-09-11 18:57:51 -0300

    fixing some things with cards

--- app/client/public/scripts/components/Card.js
+++ app/client/public/scripts/components/Card.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import ResultList from './Results';
 
 export default class Card extends Component {
+    // try to finish this tomorrow
+    // make the favorite button and send the name to the server, like an API call
+    // make a real good looking card 
     render() {
         return (
             <div id="card">
                 <div id="openstreet"></div>
-                <div id="photo"><img src={this.props.info.image_url} height="180" width="180" /></div>
+                <div id="photo"><img src={this.props.info.image_url} height="160" width="160" /></div>
                 <div id="info">
                     <ul>
                         <li> name :{this.props.info.name}</li>
